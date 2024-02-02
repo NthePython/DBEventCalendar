b9538de4227897d573180268a6dce67882b9081a
TheN <max.nurislom@gmail.com>
2024-02-02 11:56:40 +0500

    fixed Participants findByUserId and findByEventId methods

--- src/main/java/uz/eventmngmnt/event_management/repository/ParticipantsRepository.java
+++ src/main/java/uz/eventmngmnt/event_management/repository/ParticipantsRepository.java
@@ -4,10 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import uz.eventmngmnt.event_management.entity.Participants;
 import uz.eventmngmnt.event_management.entity.enums.Roles;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ParticipantsRepository extends JpaRepository<Participants, Long> {
     Optional<Participants> findByEventIdAndRole(Long eventId, Roles role);
-    Optional<Participants> findByUserId(Long userId);
-    Optional<Participants> findByEventId(Long eventId);
+    List<Participants> findByUserId(Long userId);
+    List<Participants> findByEventId(Long eventId);
 }
